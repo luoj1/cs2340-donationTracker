a3afa971c34105a64aad06245f6106ff9210fe28
luoj1 <2404551877@qq.com>
2018-09-20 20:03:56 -0400

    architect

---
+++ app/src/main/java/com/example/jamesluo/donationtracker/Model.java
@@ -0,0 +1,21 @@
+package com.example.jamesluo.donationtracker;
+
+/**
+ * Created by jamesluo on 9/20/18.
+ */
+
+public class Model {
+    private final static String user = "user";
+    private final static String pwd = "pass";
+
+    public static boolean verify(String u, String p){
+        if (u == user && p == pwd){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+
+
+}
