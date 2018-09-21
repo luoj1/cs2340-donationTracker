f9a7a2e9565c3245635e3c97cb6e90907325317d
shenshaohua <shnice_shen@sina.com>
2018-09-21 14:01:33 -0400

    welcome page

--- app/src/main/java/com/example/jamesluo/donationtracker/Welcome.java
+++ app/src/main/java/com/example/jamesluo/donationtracker/Welcome.java
@@ -2,6 +2,8 @@ package com.example.jamesluo.donationtracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Welcome extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class Welcome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+
     }
 }
