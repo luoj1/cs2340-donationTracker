b44f7aa08c949751c45d0338a52dfb8d02a53f4a
shenshaohua <shnice_shen@sina.com>
2018-09-22 15:35:45 -0400

    logout working now

--- app/src/main/java/com/example/jamesluo/donationtracker/LoginSuccess.java
+++ app/src/main/java/com/example/jamesluo/donationtracker/LoginSuccess.java
@@ -1,11 +1,13 @@
 package com.example.jamesluo.donationtracker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class LoginSuccess extends AppCompatActivity {
 
@@ -13,15 +15,12 @@ public class LoginSuccess extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_success);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button logout = (Button) findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent i = new Intent(LoginSuccess.this, MainActivity.class);
+                startActivity(i);
             }
         });
     }
