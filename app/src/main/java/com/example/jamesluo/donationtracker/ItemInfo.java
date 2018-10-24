7a3c197f999d936c92890b752eba433e40b399bd
luoj1 <2404551877@qq.com>
2018-10-23 20:45:09 -0400

    item listview

---
+++ app/src/main/java/com/example/jamesluo/donationtracker/ItemInfo.java
@@ -0,0 +1,10 @@
+package com.example.jamesluo.donationtracker;
+
+import android.app.Activity;
+
+/**
+ * Created by jamesluo on 10/23/18.
+ */
+
+public class ItemInfo extends Activity {
+}
--- app/src/main/java/com/example/jamesluo/donationtracker/LocationInfo.java
+++ app/src/main/java/com/example/jamesluo/donationtracker/LocationInfo.java
@@ -1,10 +1,17 @@
 package com.example.jamesluo.donationtracker;
 
 import android.app.Activity;
+
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -48,5 +55,36 @@ public class LocationInfo extends Activity {
                 startActivity(i);
             }
         });
+
+        final ListView listview = (ListView) findViewById(R.id.itemlist);
+        if (Model.getItems(name) != null) {
+            String[] values = new String[Model.getItems(name).size()];
+            int tracker =0;
+            for (Item i : Model.getItems(name)) {
+                values[tracker] = Model.getItems(name).get(tracker).getItem().get("Short Description");
+                tracker ++ ;
+            }
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_list_item_1, values);
+            listview.setAdapter(adapter);
+        }
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                //change to item
+                Intent in = new Intent(LocationInfo.this, ItemInfo.class);
+                in.putExtra("id", getIntent().getStringExtra("id"));
+                in.putExtra("Name", Model.getLocations().get(position).getLocation().get("Name"));
+                in.putExtra("Type", Model.getLocations().get(position).getLocation().get("Type"));
+                in.putExtra("Longitude", Model.getLocations().get(position).getLocation().get("Longitude"));
+                in.putExtra("Latitude", Model.getLocations().get(position).getLocation().get("Latitude"));
+                in.putExtra("Address", Model.getLocations().get(position).getLocation().get("Street Address"));
+                in.putExtra("Phone", Model.getLocations().get(position).getLocation().get("Phone"));
+                startActivity(in);
+            }
+        });
+
     }
 }
