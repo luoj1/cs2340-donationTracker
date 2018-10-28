24a6e3bf1531a3e99cfe4893736f4bb632c06133
luoj1 <2404551877@qq.com>
2018-10-28 15:57:21 -0400

    init server model

---
+++ app/src/main/java/com/example/jamesluo/donationtracker/ServerModel.java
@@ -0,0 +1,14 @@
+package com.example.jamesluo.donationtracker;
+
+import android.app.Activity;
+
+/**
+ * Created by jamesluo on 10/25/18.
+ */
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+public class ServerModel {
+
+
+}
