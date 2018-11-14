0b717a64d12147dfd889971ea16b6b7d496b96db
shenshaohua <shnice_shen@sina.com>
2018-11-13 19:51:41 -0500

    getItems test

--- app/src/test/java/com/example/jamesluo/donationtracker/GetItemsTest.java
+++ app/src/test/java/com/example/jamesluo/donationtracker/GetItemsTest.java
@@ -7,9 +7,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
-public class SetItemsTest {
+public class GetItemsTest {
     private static HashMap<String, List<Item>> testItems = Model.getItems();
     private String location1 = "Atlanta";
     private String location2 = "Golden State";
@@ -17,7 +18,7 @@ public class SetItemsTest {
     private Map<String, String> l2 = new HashMap<>();
     private Map<String, String> l3 = new HashMap<>();
     @Test
-    public void setItemsTest() {
+    public void getItemsTest() {
         l1.put("a","a");
         l2.put("b","b");
         l3.put("c","c");
@@ -31,12 +32,9 @@ public class SetItemsTest {
         items2.add(item2);
         items2.add(item1);
         testItems.put(location2, items2);
-        assertTrue(testItems.containsKey("Golden State"));
-        assertFalse(testItems.containsKey("Atlanta"));
-        Model.setItems(location1, item2,testItems);
-        Model.setItems(location2, item3,testItems);
-        assertTrue(testItems.containsKey("Atlanta"));
-        assertTrue(testItems.get("Atlanta").contains(item2));
-        assertTrue(testItems.get("Golden State").contains(item3));
+        List<Item> result = Model.getItems(location1);
+        assertNull(result);
+        result = Model.getItems(location2);
+        assertEquals(result, items2);
     }
 }
